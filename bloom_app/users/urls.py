fc3ad735a1e6c2f0a49f227610e817d674f530d6
SUBEEN SEO <76618706+marchsnowrabbit@users.noreply.github.com>
2024-10-08 14:15:39 +0900

    Delete bloom_app directory

--- bloom_app/users/urls.py
+++
@@ -1,8 +0,0 @@
-# users/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('profile/', views.profile, name='profile'),
-]
