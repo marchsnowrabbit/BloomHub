568d0a6216b6bfeab3a97487642eaceda261bd65
LeeEunAh <p300648@donga.ac.kr>
2024-11-08 09:49:28 +0900

    한국시간으로 변경

--- Bloom_hub/Bloom_hub/settings.py
+++ Bloom_hub/Bloom_hub/settings.py
@@ -106,9 +106,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = "Asia/Seoul"
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = 'Asia/Seoul' 
+
+USE_TZ = True
 
 USE_I18N = True
 
